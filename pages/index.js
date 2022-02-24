98cb836513adfcbc4bdc0e620b86018cccc83ec3
AnwarHossainSR <mahedianwar@gmail.com>
2022-02-24 13:13:31 +0600

    Routing with Pages

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
-import styles from "../styles/Home.module.css";
+import React from "react";
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <h1>Hello World</h1>
-    </div>
-  );
-}
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
