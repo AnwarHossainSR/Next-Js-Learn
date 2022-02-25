02b0e2c398b44bbd268c1e5d1bd70c32c344bfe5
AnwarHossainSR <mahedianwar@gmail.com>
2022-02-25 22:54:41 +0600

    Link Component Navigation

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div>Home</div>
+      <Link href="/blogs">Blogs</Link>
+      <br />
+      <Link href="/products">Products</Link>
+    </>
+  );
 };
 
 export default Home;
